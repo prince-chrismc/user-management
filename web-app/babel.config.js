2f02a80e128bd279eb01824ad550dd363d86f5d3
Christopher McArthur <prince.chrismc@gmail.com>
2020-06-20 21:49:02 -0400

    :test_tube: trying to add some test code with jest

---
+++ web-app/babel.config.js
@@ -0,0 +1,16 @@
+module.exports = {
+   presets: [
+       [
+           '@babel/preset-env',
+           {
+               modules: 'commonjs',
+               debug: false
+           }
+       ],
+       '@babel/preset-react'
+   ],
+   plugins: [
+       '@babel/plugin-syntax-dynamic-import',
+       '@babel/plugin-proposal-class-properties'
+   ]
+};
