96849bac07ddbe3ce5130a6e20d000443cc28fac
Christopher McArthur <prince.chrismc@gmail.com>
2020-06-20 21:57:34 -0400

    :construction_worker_man: adding basic user test

---
+++ web-app/test/user.test.js
@@ -0,0 +1,27 @@
+import React from "react";
+import { render, unmountComponentAtNode } from "react-dom";
+import { act } from "react-dom/test-utils";
+
+import User from '../src/components/User'
+
+let container = null;
+beforeEach(() => {
+   // setup a DOM element as a render target
+   container = document.createElement("div");
+   document.body.appendChild(container);
+});
+
+afterEach(() => {
+   // cleanup on exiting
+   unmountComponentAtNode(container);
+   container.remove();
+   container = null;
+});
+
+it("renders", () => {
+   act(() => {
+      render(<User name="Jenny Doe" email="jenny@example.com"/>, container);
+   });
+   expect(container.textContent).toContain("Jenny Doe");
+   expect(container.textContent).toContain("jenny@example.com");
+});
