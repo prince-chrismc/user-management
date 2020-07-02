ee3e2c1a6e9a528e56741d16bfb84fc8eff3728a
Christopher McArthur <prince.chrismc@gmail.com>
2020-07-02 19:44:26 -0400

    :construction: moving cmake setup to seperate files

---
+++ backend/cmake/unit-test-helper.cmake
@@ -0,0 +1,11 @@
+set(UNIT_TEST_HELPER "${CMAKE_BINARY_DIR}/utility/unit_test/helpers.hpp")
+if(NOT EXISTS ${UNIT_TEST_HELPER})
+  set(UNIT_TEST_HELPER_URL
+      "https://raw.githubusercontent.com/Stiffstream/restinio/4005036a246a7843c6ef14aa3d1fad3531240f2e/dev/test/common/pub.hpp"
+  )
+  file(DOWNLOAD ${UNIT_TEST_HELPER_URL} ${UNIT_TEST_HELPER})
+endif()
+
+add_library(unit_test_helper INTERFACE)
+target_include_directories(unit_test_helper
+                           INTERFACE "${CMAKE_BINARY_DIR}/utility")
