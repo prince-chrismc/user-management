665dfca239dc33d22dc979a86a8856e5bc93088d
Christopher McArthur <prince.chrismc@gmail.com>
2020-07-28 21:54:18 -0400

    :charts_with_upward_trend: outline for an edit user test

--- web-app/test/edit.user.test.js
+++ web-app/test/edit.user.test.js
@@ -8,4 +8,11 @@ it("renders", () => {
    const { getByText } = render(<User id="0" name="Jenny Doe" email="jenny@example.com" />);
    expect(getByText('Jenny Doe')).toBeInTheDocument()
    expect(getByText('jenny@example.com')).toBeInTheDocument()
+
+   fireEvent.click(screen.getByText('Edit'))
+   waitFor(() => screen.getByRole('textbox'))
+
+   expect(screen.getAllByRole('textbox')[0]).toHaveValue('Jenny Doe')
+   expect(screen.getAllByRole('textbox')[1]).toHaveValue('jenny@example.com')
+   expect(screen.getByRole('button', {name: 'Save'})).toHaveTextContent('Save')
 });
