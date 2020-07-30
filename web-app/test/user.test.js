1719776a7c25ee53d30ffcb6a32a6741b3c1c08d
Christopher McArthur <prince.chrismc@gmail.com>
2020-07-29 22:55:46 -0400

    make sure button are rendered

--- web-app/test/user.test.js
+++ web-app/test/user.test.js
@@ -5,7 +5,9 @@ import '@testing-library/jest-dom/extend-expect'
 import User from '../src/components/cards/Display'
 
 it('renders', () => {
-  const { getByText } = render(<User id="0" name="Jenny Doe" email="jenny@example.com" />)
+  const { getByText, getByRole } = render(<User id="0" name="Jenny Doe" email="jenny@example.com" />)
   expect(getByText('Jenny Doe')).toBeInTheDocument()
   expect(getByText('jenny@example.com')).toBeInTheDocument()
+  expect(getByRole('button', { name: 'Edit' })).toHaveTextContent('Edit')
+  expect(getByRole('button', { name: 'Delete' })).toHaveTextContent('Delete')
 })
