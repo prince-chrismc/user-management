7f9b1ce5915f59bc5b799936ac457ed8e5afff6d
Christopher McArthur <prince.chrismc@gmail.com>
2020-07-29 22:40:23 -0400

    :chart_with_upwards_trend: more tests

--- web-app/test/edit.user.test.js
+++ web-app/test/edit.user.test.js
@@ -1,36 +1,64 @@
-import React from "react";
+import React from 'react'
 import { render, fireEvent, waitFor, screen } from '@testing-library/react'
 import '@testing-library/jest-dom/extend-expect'
 import waitForExpect from 'wait-for-expect'
-
-jest.mock('../src/components/endpoints/User'); // to override API calls to backend
+import userEvent from '@testing-library/user-event' // to override API calls to backend
 
 import User from '../src/components/cards/Display'
 import ModifyUser from '../src/components/user/Edit'
 
-test("renders", () => {
-   render(<User id="0" name="Jenny Doe" email="jenny@example.com" />);
+jest.mock('../src/components/endpoints/User')
 
-   fireEvent.click(screen.getByText('Edit'))
-   waitFor(() => screen.getByRole('button', { name: 'Save' }))
+test('renders', () => {
+  render(<User id="0" name="Jenny Doe" email="jenny@example.com" />)
 
-   expect(screen.getAllByRole('textbox')[0]).toHaveValue('Jenny Doe')
-   expect(screen.getAllByRole('textbox')[1]).toHaveValue('jenny@example.com')
-   expect(screen.getByRole('button', { name: 'Save' })).toHaveTextContent('Save')
-});
+  fireEvent.click(screen.getByText('Edit'))
+  waitFor(() => screen.getByRole('button', { name: 'Save' }))
 
-test("default data on submit", () => {
-   const mockCallback = jest.fn((name, email) => { })
-   const { getByText, getByRole } = render(<ModifyUser id="0" name="Jenny Doe" email="jenny@example.com"
-      onChange={mockCallback} />)
+  expect(screen.getByPlaceholderText('Name')).toHaveValue('Jenny Doe')
+  expect(screen.getByPlaceholderText('Email')).toHaveValue('jenny@example.com')
+  expect(screen.getByRole('button', { name: 'Save' })).toHaveTextContent('Save')
 
-   fireEvent.click(screen.getByText('Edit'))
-   waitFor(() => getByRole('button', { name: 'Save' }))
-   fireEvent.click(getByRole('button', { name: 'Save' }))
+  userEvent.type(screen.getByPlaceholderText('Name'), '{esc}')
 
-   waitForExpect(() => {
-      expect(mockCallback.mock.calls.length).toBe(1)
-      expect(mockCallback.mock.calls[0][0]).toBe("Jenny Doe");
-      expect(mockCallback.mock.calls[0][0]).toBe("jenny@example.com");
-   })
-});
+  expect(screen.queryByPlaceholderText('Name')).not.toBeInTheDocument()
+  expect(screen.queryByPlaceholderText('Email')).not.toBeInTheDocument()
+  expect(screen.queryByRole('button', { name: 'Save' })).not.toBeInTheDocument()
+})
+
+test('default data on submit', () => {
+  const mockCallback = jest.fn((name, email) => { })
+  const { getByText, getByRole } = render(<ModifyUser id="0" name="Jenny Doe" email="jenny@example.com"
+    onChange={mockCallback} />)
+
+  fireEvent.click(screen.getByText('Edit'))
+  waitFor(() => getByRole('button', { name: 'Save' }))
+  fireEvent.click(getByRole('button', { name: 'Save' }))
+
+  waitForExpect(() => {
+    expect(mockCallback.mock.calls.length).toBe(1)
+    expect(mockCallback.mock.calls[0][0]).toBe('Jenny Doe')
+    expect(mockCallback.mock.calls[0][0]).toBe('jenny@example.com')
+    expect(screen.getByRole('button', { name: 'Save' })).toBeDisabled()
+  })
+})
+
+test('new data on submit', () => {
+  const mockCallback = jest.fn((name, email) => { })
+  const { getByPlaceholderText, getByRole } = render(<ModifyUser id="0" name="Jenny Doe" email="jenny@example.com"
+    onChange={mockCallback} />)
+
+  fireEvent.click(screen.getByText('Edit'))
+  waitFor(() => getByRole('button', { name: 'Save' }))
+
+  fireEvent.change(getByPlaceholderText('Name'), { target: { value: 'John Doe' } })
+  fireEvent.change(getByPlaceholderText('Email'), { target: { value: 'john@example.com' } })
+
+  fireEvent.click(getByRole('button', { name: 'Save' }))
+
+  waitForExpect(() => {
+    expect(mockCallback.mock.calls.length).toBe(1)
+    expect(mockCallback.mock.calls[0][0]).toBe('John Doe')
+    expect(mockCallback.mock.calls[0][0]).toBe('john@example.com')
+  })
+})
--- web-app/test/user.test.js
+++ web-app/test/user.test.js
@@ -1,11 +1,11 @@
-import React from "react";
+import React from 'react'
 import { render } from '@testing-library/react'
 import '@testing-library/jest-dom/extend-expect'
 
 import User from '../src/components/cards/Display'
 
-it("renders", () => {
-   const { getByText } = render(<User id="0" name="Jenny Doe" email="jenny@example.com" />);
-   expect(getByText('Jenny Doe')).toBeInTheDocument()
-   expect(getByText('jenny@example.com')).toBeInTheDocument()
-});
+it('renders', () => {
+  const { getByText } = render(<User id="0" name="Jenny Doe" email="jenny@example.com" />)
+  expect(getByText('Jenny Doe')).toBeInTheDocument()
+  expect(getByText('jenny@example.com')).toBeInTheDocument()
+})
