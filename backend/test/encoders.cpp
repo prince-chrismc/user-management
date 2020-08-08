41d3826bb746c2e67ccea444cc1e86e41f2ccbe7
Christopher McArthur <prince.chrismc@gmail.com>
2020-08-07 21:46:00 -0400

    firgured our how to test the sha256
    https://www.di-mgt.com.au/sha_testvectors.html

--- backend/test/encoders.cpp
+++ backend/test/encoders.cpp
@@ -2,15 +2,20 @@
 
 #include <catch2/catch.hpp>
 
+#include "encoders/base64.hpp"
 #include "encoders/sha256.hpp"
 
 #include <iostream>
 
+TEST_CASE("base64") {
+  const std::string input{"1"};
+  CHECK(encode::base64(input.data(), input.length()) == "MQ==");
+}
+
 TEST_CASE("sha256") {
-  const std::string input{"hello world"};
-  const auto data = encode::sha256(input.data(), input.length());
-  for (char c : data) {
-    std::cout << (unsigned int)c << std::endl;
-  }
-  CHECK(encode::sha256(input.data(), input.length()) == "\x02"); // TODO: Fix check
+  const std::string input{"abc"};
+  CHECK(encode::sha256(input.data(), input.length()) == std::string{"\xba\x78\x16\xbf\x8f\x01\xcf\xea\x41\x41\x40"
+                                                                    "\xde\x5d\xae\x22\x23\xb0\x03\x61\xa3\x96\x17"
+                                                                    "\x7a\x9c\xb4\x10\xff\x61\xf2\x00\x15\xad",
+                                                                    32});
 }
