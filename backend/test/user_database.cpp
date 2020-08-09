3e0cbf05a5b8f2f2250434804ba80a4b7e9bcda0
Christopher McArthur <prince.chrismc@gmail.com>
2020-08-09 19:29:37 -0400

    test to check the etag value

--- backend/test/user_database.cpp
+++ backend/test/user_database.cpp
@@ -8,28 +8,48 @@
 namespace Catch {
 template <>
 struct StringMaker<user::database::time_point> {
-  static std::string convert(std::chrono::system_clock::time_point const& value) { return fmt::format("{}", value.time_since_epoch()); }
+  static std::string convert(std::chrono::system_clock::time_point const& value) {
+    return fmt::format("{}", value.time_since_epoch());
+  }
 };
 }  // namespace Catch
 
 TEST_CASE("Tracks Last-Modified") {
   user::database user_database;
-  
+
   // Add
   const auto new_id = user_database.add(R"##({"name": "Jane Doe", "email": "jane@example.com"})##"_json).id;
   CHECK(user_database.last_modified() == user_database.last_modified(new_id));
-  
+
   // Edit
   user_database.edit(new_id, R"##({"name": "Jane Down"})##"_json);
   CHECK(user_database.last_modified() == user_database.last_modified(new_id));
-  
+
   // Multiple Users
   const auto second_id = user_database.add(R"##({"name": "John Doe", "email": "john@example.com"})##"_json).id;
   CHECK(user_database.last_modified() == user_database.last_modified(second_id));
   CHECK_FALSE(user_database.last_modified() == user_database.last_modified(new_id));
-  
+
   // Remove
   user_database.remove(new_id);
   CHECK(user_database.last_modified() == user_database.last_modified(new_id));
-  CHECK_FALSE(user_database.last_modified() == user_database.last_modified(second_id));  
+  CHECK_FALSE(user_database.last_modified() == user_database.last_modified(second_id));
+}
+
+TEST_CASE("Handles ETAg") {
+  user::database user_database;
+  const auto db_empty_hash = user_database.etag();
+
+  // Add
+  const auto new_id = user_database.add(R"##({"name": "Jane Doe", "email": "jane@example.com"})##"_json).id;
+  CHECK(user_database.etag() != db_empty_hash);
+  const auto new_user_hash = user_database.etag(new_id);
+
+  // Edit
+  user_database.edit(new_id, R"##({"name": "Jane Down"})##"_json);
+  CHECK(user_database.etag() != db_empty_hash);
+  CHECK(user_database.etag(new_id) != new_user_hash);
+
+  user_database.remove(new_id);
+  CHECK(user_database.etag() == db_empty_hash);
 }
