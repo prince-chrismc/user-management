a799e785b50d7cbc770f80954146e0613b7b787e
Chris Mc <prince.chrismc@gmail.com>
2020-08-09 11:36:10 -0400

    adding tests for last modified on remove user

--- backend/test/user_database.cpp
+++ backend/test/user_database.cpp
@@ -1,7 +1,6 @@
 // MIT License
 
 #include <fmt/chrono.h>
-
 #include <catch2/catch.hpp>
 
 #include "database/users.hpp"
@@ -13,13 +12,24 @@ struct StringMaker<user::database::time_point> {
 };
 }  // namespace Catch
 
-TEST_CASE("Tracks changes") {
+TEST_CASE("Tracks Last-Modified") {
   user::database user_database;
+  
+  // Add
   const auto new_id = user_database.add(R"##({"name": "Jane Doe", "email": "jane@example.com"})##"_json).id;
   CHECK(user_database.last_modified() == user_database.last_modified(new_id));
+  
+  // Edit
   user_database.edit(new_id, R"##({"name": "Jane Down"})##"_json);
   CHECK(user_database.last_modified() == user_database.last_modified(new_id));
+  
+  // Multiple Users
   const auto second_id = user_database.add(R"##({"name": "John Doe", "email": "john@example.com"})##"_json).id;
   CHECK(user_database.last_modified() == user_database.last_modified(second_id));
   CHECK_FALSE(user_database.last_modified() == user_database.last_modified(new_id));
+  
+  // Remove
+  user_database.remove(new_id);
+  CHECK(user_database.last_modified() == user_database.last_modified(new_id));
+  CHECK_FALSE(user_database.last_modified() == user_database.last_modified(second_id));  
 }
