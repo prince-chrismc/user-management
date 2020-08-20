8cb665271883caacc7e13a5fc62e0d2571117cba
Chris Mc <prince.chrismc@gmail.com>
2020-08-19 23:21:27 -0400

    [wip] adding error handling test code

--- backend/test/user_routes.cpp
+++ backend/test/user_routes.cpp
@@ -1,10 +1,11 @@
 // MIT License
 
 #include "handlers/user_routes.hpp"
-#include "unit_test/helpers.hpp"
 
 #include <catch2/catch.hpp>
 #include <restinio/all.hpp>
+
+#include "unit_test/helpers.hpp"
 
 using router_t = restinio::router::express_router_t<>;
 using traits_t = restinio::traits_t<restinio::asio_timer_manager_t, restinio::null_logger_t, router_t>;
@@ -38,9 +39,25 @@ TEST_CASE("List Endpoints") {
     CHECK_THAT(response, Catch::Contains(nlohmann::json(list).dump()));
   }
 
-  SECTION("POST") {
-    const std::string post_list{
-        "POST /um/v1/users HTTP/1.0\r\n"
+  SECTION("GET 304") {
+    const std::string get_list{
+        "GET /um/v1/users HTTP/1.0\r\n"
+        "From: unit-test\r\n"
+        "User-Agent: unit-test\r\n"
+        "If-None-Match: \"" +
+        list.etag() +
+        "\"\r\n"
+        "Connection: close\r\n"
+        "\r\n"};
+
+    std::string response;
+    REQUIRE_NOTHROW(response = do_request(get_list));
+    CHECK_THAT(response, Catch::Contains("304") && Catch::Contains("Not Modified") && Catch::EndsWith("\r\n\r\n"));
+  }
+
+  SECTION("PUT") {
+    const std::string put_list{
+        "PUT /um/v1/users HTTP/1.0\r\n"
         "From: unit-test\r\n"
         "User-Agent: unit-test\r\n"
         "Content-Type: application/json\r\n"
@@ -50,9 +67,27 @@ TEST_CASE("List Endpoints") {
         R"##({"name": "Jane Doe", "email": "jane@example.com"})##"};
 
     std::string response;
-    REQUIRE_NOTHROW(response = do_request(post_list));
+    REQUIRE_NOTHROW(response = do_request(put_list));
     CHECK_THAT(response,
                Catch::Contains(nlohmann::json(user_management::user{2, "Jane Doe", "jane@example.com"}).dump()));
+  }
+
+  SECTION("PUT 415") {
+    const std::string put_list{
+        "PUT /um/v1/users HTTP/1.0\r\n"
+        "From: unit-test\r\n"
+        "User-Agent: unit-test\r\n"
+        "Content-Type: text/plain\r\n"
+        "Content-Length: 49\r\n"
+        "Connection: close\r\n"
+        "\r\n"
+        R"##({"name": "Jane Doe", "email": "jane@example.com"})##"};
+
+    std::string response;
+    REQUIRE_NOTHROW(response = do_request(put_list));
+    CHECK_THAT(response, Catch::Contains("415") && Catch::Contains("Unsupported Media Type") &&
+                             Catch::Contains("error") && Catch::Contains("Content-Type") &&
+                             Catch::Contains("application/json"));
   }
 }
 
@@ -71,7 +106,7 @@ TEST_CASE("User Endpoints") {
   other_work_thread_for_server_t<http_server_t> other_thread{http_server};
   other_thread.run();
 
-  SECTION("GET 200") {
+  SECTION("GET") {
     const std::string get_user{
         "GET /um/v1/users/1 HTTP/1.0\r\n"
         "From: unit-test\r\n"
@@ -84,6 +119,22 @@ TEST_CASE("User Endpoints") {
     CHECK_THAT(response, Catch::Contains(nlohmann::json(user).dump()));
   }
 
+  SECTION("GET 304") {
+    const std::string get_user{
+        "GET /um/v1/users/1 HTTP/1.0\r\n"
+        "From: unit-test\r\n"
+        "User-Agent: unit-test\r\n"
+        "If-None-Match: \"" +
+        list.etag(1) +
+        "\"\r\n"
+        "Connection: close\r\n"
+        "\r\n"};
+
+    std::string response;
+    REQUIRE_NOTHROW(response = do_request(get_user));
+    CHECK_THAT(response, Catch::Contains("304") && Catch::Contains("Not Modified") && Catch::EndsWith("\r\n\r\n"));
+  }
+
   SECTION("GET 404") {
     const std::string get_user{
         "GET /um/v1/users/0 HTTP/1.0\r\n"
@@ -94,12 +145,12 @@ TEST_CASE("User Endpoints") {
 
     std::string response;
     REQUIRE_NOTHROW(response = do_request(get_user));
-    CHECK_THAT(response, Catch::Contains("404") && Catch::Contains("Not Found") && Catch::Contains("error") /*&&
-                             Catch::Contains("user does not exists")*/);
+    CHECK_THAT(response, Catch::Contains("404") && Catch::Contains("Not Found") && Catch::Contains("error") &&
+                             Catch::Contains("user '0' does not exists"));
   }
 
-  SECTION("DELETE") {
-    const std::string get_user{
+  SECTION("DELETE 428") {
+    const std::string del_user{
         "DELETE /um/v1/users/1 HTTP/1.0\r\n"
         "From: unit-test\r\n"
         "User-Agent: unit-test\r\n"
@@ -107,12 +158,62 @@ TEST_CASE("User Endpoints") {
         "\r\n"};
 
     std::string response;
-    REQUIRE_NOTHROW(response = do_request(get_user));
+    REQUIRE_NOTHROW(response = do_request(del_user));
+    CHECK_THAT(response, Catch::Contains("428") && Catch::Contains("Precondition Required") &&
+                             Catch::Contains("error") &&
+                             Catch::Contains("an 'If-Match <ETag>' header must be provided"));
+  }
+
+  SECTION("DELETE 412") {
+    const std::string del_user{
+        "DELETE /um/v1/users/1 HTTP/1.0\r\n"
+        "From: unit-test\r\n"
+        "User-Agent: unit-test\r\n"
+        "If-Match \"dlfvbgayruefgba743t6374hnvjdbnq74gp\"\r\n"
+        "Connection: close\r\n"
+        "\r\n"};
+
+    std::string response;
+    REQUIRE_NOTHROW(response = do_request(del_user));
+    CHECK_THAT(response, Catch::Contains("412") && Catch::Contains("Precondition Failed") && Catch::Contains("error") &&
+                             Catch::Contains("the user '1' was modified without you knowledge"));
+  }
+
+  SECTION("DELETE 404") {
+    const std::string del_user{
+        "DELETE /um/v1/users/0 HTTP/1.0\r\n"
+        "From: unit-test\r\n"
+        "User-Agent: unit-test\r\n"
+        "If-Match: \"" +
+        list.etag(1) +
+        "\"\r\n"
+        "Connection: close\r\n"
+        "\r\n"};
+
+    std::string response;
+    REQUIRE_NOTHROW(response = do_request(del_user));
+    CHECK_THAT(response, Catch::Contains("404") && Catch::Contains("Not Found") && Catch::Contains("error") &&
+                             Catch::Contains("user '0' does not exists"));
+  }
+
+  SECTION("DELETE") {
+    const std::string del_user{
+        "DELETE /um/v1/users/1 HTTP/1.0\r\n"
+        "From: unit-test\r\n"
+        "User-Agent: unit-test\r\n"
+        "If-Match: \"" +
+        list.etag(1) +
+        "\"\r\n"
+        "Connection: close\r\n"
+        "\r\n"};
+
+    std::string response;
+    REQUIRE_NOTHROW(response = do_request(del_user));
     CHECK_THAT(response, Catch::Contains("204"));
   }
 
-  SECTION("PATCH") {
-    const std::string post_list{
+  SECTION("PATCH 428") {
+    const std::string patch_list{
         "PATCH /um/v1/users/1 HTTP/1.0\r\n"
         "From: unit-test\r\n"
         "User-Agent: unit-test\r\n"
@@ -123,8 +224,85 @@ TEST_CASE("User Endpoints") {
         R"##({"name": "Jane Doe", "email": "jane@example.com"})##"};
 
     std::string response;
-    REQUIRE_NOTHROW(response = do_request(post_list));
+    REQUIRE_NOTHROW(response = do_request(patch_list));
+    CHECK_THAT(response, Catch::Contains("428") && Catch::Contains("Precondition Required") &&
+                             Catch::Contains("error") &&
+                             Catch::Contains("an 'If-Match <ETag>' header must be provided"));
+  }
+
+  SECTION("PATCH 412") {
+    const std::string patch_list{
+        "PATCH /um/v1/users/1 HTTP/1.0\r\n"
+        "From: unit-test\r\n"
+        "User-Agent: unit-test\r\n"
+        "If-Match \"dlfvbgayruefgba743t6374hnvjdbnq74gp\"\r\n"
+        "Content-Type: application/json\r\n"
+        "Content-Length: 49\r\n"
+        "Connection: close\r\n"
+        "\r\n"
+        R"##({"name": "Jane Doe", "email": "jane@example.com"})##"};
+
+    std::string response;
+    REQUIRE_NOTHROW(response = do_request(patch_list));
+    CHECK_THAT(response, Catch::Contains("412") && Catch::Contains("Precondition Failed") && Catch::Contains("error") &&
+                             Catch::Contains("the user '1' was modified without you knowledge"));
+  }
+
+  SECTION("PATCH 404") {
+    const std::string patch_list{
+        "PATCH /um/v1/users/0 HTTP/1.0\r\n"
+        "From: unit-test\r\n"
+        "User-Agent: unit-test\r\n"
+        "If-Match: \"" +
+        list.etag(1) +
+        "\"\r\n"
+        "Content-Type: application/json\r\n"
+        "Content-Length: 49\r\n"
+        "Connection: close\r\n"
+        "\r\n"
+        R"##({"name": "Jane Doe", "email": "jane@example.com"})##"};
+
+    std::string response;
+    REQUIRE_NOTHROW(response = do_request(patch_list));
+    CHECK_THAT(response, Catch::Contains("404") && Catch::Contains("Not Found") && Catch::Contains("error") &&
+                             Catch::Contains("user '0' does not exists"));
+  }
+
+  SECTION("PATCH") {
+    const std::string patch_list{
+        "PATCH /um/v1/users/1 HTTP/1.0\r\n"
+        "From: unit-test\r\n"
+        "User-Agent: unit-test\r\n"
+        "If-Match: \"" +
+        list.etag(1) +
+        "\"\r\n"
+        "Content-Type: application/json\r\n"
+        "Content-Length: 49\r\n"
+        "Connection: close\r\n"
+        "\r\n"
+        R"##({"name": "Jane Doe", "email": "jane@example.com"})##"};
+
+    std::string response;
+    REQUIRE_NOTHROW(response = do_request(patch_list));
     CHECK_THAT(response,
                Catch::Contains(nlohmann::json(user_management::user{1, "Jane Doe", "jane@example.com"}).dump()));
+  }
+
+  SECTION("PATCH 415") {
+    const std::string patch_list{
+        "PATCH /um/v1/users/1 HTTP/1.0\r\n"
+        "From: unit-test\r\n"
+        "User-Agent: unit-test\r\n"
+        "Content-Type: text/plain\r\n"
+        "Content-Length: 49\r\n"
+        "Connection: close\r\n"
+        "\r\n"
+        R"##({"name": "Jane Doe", "email": "jane@example.com"})##"};
+
+    std::string response;
+    REQUIRE_NOTHROW(response = do_request(patch_list));
+    CHECK_THAT(response, Catch::Contains("415") && Catch::Contains("Unsupported Media Type") &&
+                             Catch::Contains("error") && Catch::Contains("Content-Type") &&
+                             Catch::Contains("application/json"));
   }
 }
