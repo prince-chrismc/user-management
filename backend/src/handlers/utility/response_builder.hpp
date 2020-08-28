d537505488e63464b81c46ab9847c707efbf9fbd
Christopher McArthur <prince.chrismc@gmail.com>
2020-08-27 21:16:41 -0400

    move response builder impl to cpp

--- backend/src/handlers/utility/response_builder.hpp
+++ backend/src/handlers/utility/response_builder.hpp
@@ -10,45 +10,21 @@ using http_status_line = restinio::http_status_line_t;
 
 class builder;
 namespace impl {
-inline void add_generic_headers(builder& builder);
-inline void add_cors_headers(builder& builder);
-inline void add_api_headers(builder& builder);
+void add_generic_headers(builder& builder);
+void add_cors_headers(builder& builder);
+void add_api_headers(builder& builder);
 }  // namespace impl
 
 class builder {
   restinio::response_builder_t<restinio::restinio_controlled_output_t> builder_;
 
  public:
-  builder(const request_handle& req, const http_status_line& status) : builder_{req->create_response(status)} {
-    impl::add_generic_headers(*this);
-    impl::add_cors_headers(*this);
+  builder(const request_handle& req, const http_status_line& status);
+  builder(const request_handle& req);
 
-    // Some responses codes do not require the API Content-Type header
-    switch (status.status_code().raw_code()) {
-      case restinio::status_code::no_content.raw_code():
-      case restinio::status_code::not_modified.raw_code():
-        break;
-      default:
-        impl::add_api_headers(*this);
-    }
-  }
-
-  builder(const request_handle& req) : builder(req, restinio::status_ok()) {}
-
-  builder& append_header(http_field field_id, std::string field_value) {
-    builder_.append_header(field_id, std::move(field_value));
-    return *this;
-  }
-
-  builder& append_header(http_field field_id, std::chrono::system_clock::time_point tp) {
-    builder_.append_header(field_id, restinio::make_date_field_value(tp));
-    return *this;
-  }
-
-  builder& set_body(std::string body) {
-    builder_.set_body(std::move(body));
-    return *this;
-  }
+  builder& append_header(http_field field_id, std::string field_value);
+  builder& append_header(http_field field_id, std::chrono::system_clock::time_point tp);
+  builder& set_body(std::string body);
 
   auto done() { return builder_.done(); }
 };
@@ -66,65 +42,36 @@ class error_builder : builder {
   }
 };
 
-class not_found : public error_builder<user_management::user_does_not_exist> {
- public:
+struct not_found : error_builder<user_management::user_does_not_exist> {
   not_found(const request_handle& req) : error_builder(req, restinio::status_not_found()){};
 };
 
-class unsupported_media_type : public error_builder<> {
- public:
+struct unsupported_media_type : error_builder<std::runtime_error> {
   unsupported_media_type(const request_handle& req) : error_builder(req, restinio::status_unsupported_media_type()){};
 };
 
-class precondition_required : public error_builder<> {
- public:
+struct precondition_required : error_builder<std::runtime_error> {
   precondition_required(const request_handle& req) : error_builder(req, restinio::status_precondition_required()) {}
 };
 
-class precondition_failed : public error_builder<> {
- public:
+struct precondition_failed : error_builder<std::runtime_error> {
   precondition_failed(const request_handle& req) : error_builder(req, restinio::status_precondition_failed()) {}
 };
 
 namespace builders {
-class list : public builder {
- public:
-  static void add_cors_methods_header(builder& builder) {
-    builder.append_header(http_field::access_control_allow_methods, "GET, PUT");
-  }
+struct list : builder {
+  list(const request_handle& req, const http_status_line& status);
+  list(const request_handle& req);
 
-  list(const request_handle& req, const http_status_line& status) : builder(req, status) {
-    add_cors_methods_header(*this);
-  }
-
-  list(const request_handle& req) : list(req, restinio::status_ok()) {}
+  static void add_cors_methods_header(builder& builder);
 };
 
-class user : public builder {
- public:
-  static void add_cors_methods_header(builder& builder) {
-    builder.append_header(http_field::access_control_allow_methods, "GET, PATCH, DELETE");
-  }
+struct user : builder {
+  user(const request_handle& req, const http_status_line& status);
+  user(const request_handle& req);
 
-  user(const request_handle& req, const http_status_line& status) : builder(req, status) {
-    add_cors_methods_header(*this);
-  }
-
-  user(const request_handle& req) : user(req, restinio::status_ok()) {}
+  static void add_cors_methods_header(builder& builder);
 };
 }  // namespace builders
-
-namespace impl {
-inline void add_generic_headers(builder& builder) {
-  builder.append_header(http_field::server, "user-management/1.0.0-dev.0; restinio/0.6.8.1")
-      .append_header(http_field::date, std::chrono::system_clock::now());
-}
-inline void add_cors_headers(builder& builder) {
-  builder.append_header(http_field::access_control_allow_origin, "*")
-      .append_header(restinio::http_field::access_control_allow_headers, "Content-Type")
-      .append_header(restinio::http_field::access_control_max_age, "86400");
-}
-inline void add_api_headers(builder& builder) { builder.append_header(http_field::content_type, "application/json"); }
-}  // namespace impl
 }  // namespace response
 }  // namespace handler
