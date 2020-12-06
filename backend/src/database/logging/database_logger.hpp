c62ddafd856b98e4599b5e379f2ee5ff5c0ade75
Chris McArthur <prince.chrismc@gmail.com>
2020-12-06 10:18:30 -0500

    fixing database logger

--- backend/src/database/logging/database_logger.hpp
+++ backend/src/database/logging/database_logger.hpp
@@ -5,6 +5,10 @@
 
 #include <spdlog/spdlog.h>
 
+#if defined(CONSOLE_LOGGING) == defined(SYSLOG_LOGGING)  // Niether or both
+#error "One logging method must be configured!"
+#endif
+
 #ifdef CONSOLE_LOGGING
 #include <spdlog/sinks/stdout_color_sinks.h>
 #endif
@@ -16,12 +20,14 @@
 namespace database {
 struct logger {
   logger() {
+    if (spdlog::get("database") == nullptr) {
 #ifdef CONSOLE_LOGGING
-    spdlog::stdout_color_mt("database")->set_level(spdlog::level::trace);
+      spdlog::stdout_color_mt("database")->set_level(spdlog::level::trace);
 #endif
 #ifdef SYSLOG_LOGGING
-    spdlog::syslog_logger_mt("database", "um_database", LOG_PID)->set_level(spdlog::level::info);
+      spdlog::syslog_logger_mt("database", "um_database", LOG_PID)->set_level(spdlog::level::info);
 #endif
+    }
   }
   template <typename FormatString, typename... Args>
   inline void trace(const FormatString &fmt, const Args &... args) const {
