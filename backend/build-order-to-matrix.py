d4fb6d044d6ed36c0315790e4cedea00cec97aa6
Chris McArthur <prince.chrismc@gmail.com>
2021-01-06 23:23:13 -0500

    fix script for all deps

--- backend/build-order-to-matrix.py
+++ backend/build-order-to-matrix.py
@@ -5,8 +5,9 @@ def main():
     matrix = {"include": []}
     with open("build_order.json", "r") as read_file:
         data = json.load(read_file)
-        for reference in data[0]:
-            matrix["include"].append({"reference": reference[0]})
+        for level in data:
+            for reference in level:
+                matrix["include"].append({"reference": reference[0]})
 
         with open("matrix.json", "w") as write_file:
             json.dump(matrix, write_file)
