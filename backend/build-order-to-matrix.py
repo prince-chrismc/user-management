8a1389ee884a08e224a9862641a5dedf35fb6e16
Chris Mc <prince.chrismc@gmail.com>
2021-01-12 22:29:42 -0500

    Update build-order-to-matrix.py

--- backend/build-order-to-matrix.py
+++ backend/build-order-to-matrix.py
@@ -9,6 +9,9 @@ def main():
             for reference in level:
                 matrix["include"].append({"reference": reference[0]})
 
+        if len(matrix["include"]) == 0:
+            matrix["include"].append({"reference": "null"})
+
         with open("matrix.json", "w") as write_file:
             json.dump(matrix, write_file)
 
