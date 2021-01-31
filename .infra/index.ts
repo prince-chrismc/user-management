3cb188083942d1ea70555f9fca08323de5d3f686
Chris McArthur <prince.chrismc@gmail.com>
2021-01-31 01:12:49 -0500

    wip adding backend + building frontend with correct URL

--- .infra/index.ts
+++ .infra/index.ts
@@ -1,5 +1,23 @@
 import * as awsx from "@pulumi/awsx";
 
+// The data layer for the application
+// Use the 'image' property to point to a pre-built Docker image.
+const backendListener = new awsx.elasticloadbalancingv2.NetworkListener("backend", { port: 8080 });
+const backend = new awsx.ecs.FargateService("backend", {
+    taskDefinitionArgs: {
+        containers: {
+            backend: {
+                image: "princechrismc.jfrog.io/user-management-docker/user-management-backend:1.0.0-commit.224",
+                memory: 512,
+                portMappings: [backendListener],
+                command: ["dist", "-a", "0.0.0.0", "-p", "8080", "-n", "4"],
+            },
+        },
+    },
+});
+
+const backendEndpoint = backendListener.endpoint;
+
 // Create a load balancer to listen for requests and route them to the container.
 let lb = new awsx.lb.NetworkListener("frontend", { port: 80 });
 
@@ -9,9 +27,14 @@ let service = new awsx.ecs.FargateService("frontend", {
     taskDefinitionArgs: {
         containers: {
             frontend: {
-                image: awsx.ecs.Image.fromPath("frontend", "../web-app"),
+                image: awsx.ecs.Image.fromDockerBuild("frontend", {
+                    context: "../web-app",
+                    dockerfile: "../web-app/Dockerfile.multistage",
+                    // cacheFrom: { stages: ["install"] },
+                    args: { api_url: backendEndpoint.apply(e => `"http://${e.hostname}:${e.port.toString()}"`) }
+                }),
                 memory: 512,
-                portMappings: [ lb ],
+                portMappings: [lb],
             },
         },
     },
