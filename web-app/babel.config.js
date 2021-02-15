4b23c46f33e7f0ef8aa5064999d4ec48e9238ab9
Chris McArthur <prince.chrismc@gmail.com>
2021-02-14 21:59:03 -0500

    fix bable config's reading of env

--- web-app/babel.config.js
+++ web-app/babel.config.js
@@ -5,9 +5,9 @@ const plugins = [
   "@babel/plugin-proposal-class-properties"
 ];
 
-if (process.env["ENV"] === "prod") {
+if (process.env["NODE_ENV"] === "production") {
   presets.push(["@babel/preset-env", { modules: false }]);
-  plugins.push("transform-react-remove-prop-types")
+  plugins.push(["transform-react-remove-prop-types", { mode: 'remove', ignoreFilenames: ['node_modules'], }])
 } else {
   presets.push(["@babel/preset-env", { useBuiltIns: "usage", corejs: { version: "3.8", proposals: true } }]);
   plugins.push("@babel/plugin-transform-runtime")
