df716e311d8a68834aaa8b339741e73ce47b129e
Chris McArthur <prince.chrismc@gmail.com>
2021-02-16 20:46:41 -0500

    remove useInput

--- web-app/src/components/forms/User.js
+++ web-app/src/components/forms/User.js
@@ -1,18 +1,19 @@
+import { useState } from 'react'
 import { Form } from 'semantic-ui-react'
-import useInput from '@rooks/use-input'
+// import useInput from '@rooks/use-input'
 
 const UserForm = ({ user, handleSubmit, disabled }) => {
-  const name = useInput(user.name)
-  const email = useInput(user.email)
+  const [name, setName] = useState(user.name)
+  const [email, setEmail] = useState(user.email)
 
   return (
-        <Form onSubmit={() => { handleSubmit(name.value, email.value) }}>
-            <Form.Group>
-                <Form.Input placeholder='Name' name='name' {...name} />
-                <Form.Input placeholder='Email' name='email' {...email} />
-                <Form.Button color='green' icon='check' content='Save' inverted disabled={disabled} />
-            </Form.Group>
-        </Form>
+    <Form onSubmit={() => { handleSubmit(name.value, email.value) }}>
+      <Form.Group>
+        <Form.Input placeholder='Name' name='name' value={name} onChange={(e, { value }) => setName(value)} />
+        <Form.Input placeholder='Email' name='email' value={name} onChange={(e, { value }) => setEmail(value)} />
+        <Form.Button color='green' icon='check' content='Save' inverted disabled={disabled} />
+      </Form.Group>
+    </Form>
   )
 }
 
