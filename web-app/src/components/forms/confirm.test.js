90c91bc50864063907a5bd0384a215284b6a26ae
chris <prince.chrismc@gmail.com>
2021-02-17 22:12:44 -0500

    improve form testing

---
+++ web-app/src/components/forms/confirm.test.js
@@ -0,0 +1,34 @@
+import { render, fireEvent } from '@testing-library/react'
+import '@testing-library/jest-dom/extend-expect'
+import waitForExpect from 'wait-for-expect'
+import userEvent from '@testing-library/user-event'
+
+import ConfirmForm from './Confirm'
+
+test('renders', () => {
+  const mockCallback = jest.fn()
+  const { getByRole } = render(<ConfirmForm handleSubmit={mockCallback} />)
+
+  expect(getByRole('button', { name: 'Confirm' })).toHaveTextContent('Confirm')
+  expect(mockCallback).not.toHaveBeenCalled()
+})
+
+test('renders disabled', () => {
+  const mockCallback = jest.fn()
+  const { getByRole } = render(<ConfirmForm handleSubmit={mockCallback} disabled />)
+
+  expect(getByRole('button', { name: 'Confirm' })).toHaveTextContent('Confirm')
+  expect(getByRole('button', { name: 'Confirm' })).toBeDisabled()
+  expect(mockCallback).not.toHaveBeenCalled()
+})
+
+test('handles submissions', async () => {
+  const mockCallback = jest.fn()
+  const { getByRole } = render(<ConfirmForm handleSubmit={mockCallback} />)
+
+  userEvent.click(getByRole('button', { name: 'Confirm' }))
+
+  await waitForExpect(() => {
+    expect(mockCallback).toHaveBeenCalled()
+  }, 700)
+})
--- web-app/src/components/forms/user.test.js
+++ web-app/src/components/forms/user.test.js
@@ -5,18 +5,33 @@ import userEvent from '@testing-library/user-event'
 
 import UserForm from './User'
 
-test('renders', () => {
+test('renders enabled', () => {
   const user = { id: 0, name: 'Jenny Doe', email: 'jenny@example.com' }
-  const { getByPlaceholderText, getByRole } = render(<UserForm user={user} handleSubmit={() => { }} />)
+  const mockCallback = jest.fn()
+  const { getByPlaceholderText, getByRole } = render(<UserForm user={user} handleSubmit={mockCallback} />)
 
   expect(getByPlaceholderText('Name')).toHaveValue('Jenny Doe')
   expect(getByPlaceholderText('Email')).toHaveValue('jenny@example.com')
   expect(getByRole('button', { name: 'Save' })).toHaveTextContent('Save')
+  expect(getByRole('button', { name: 'Save' })).not.toBeDisabled()
+  expect(mockCallback).not.toHaveBeenCalled()
+})
+
+test('renders disabled', () => {
+  const user = { id: 0, name: 'Jenny Doe', email: 'jenny@example.com' }
+  const mockCallback = jest.fn()
+  const { getByPlaceholderText, getByRole } = render(<UserForm user={user} handleSubmit={mockCallback} disabled />)
+
+  expect(getByPlaceholderText('Name')).toHaveValue('Jenny Doe')
+  expect(getByPlaceholderText('Email')).toHaveValue('jenny@example.com')
+  expect(getByRole('button', { name: 'Save' })).toHaveTextContent('Save')
+  expect(getByRole('button', { name: 'Save' })).toBeDisabled()
+  expect(mockCallback).not.toHaveBeenCalled()
 })
 
 test('handles submissions', async () => {
   const user = { id: 0, name: 'Jenny Doe', email: 'jenny@example.com' }
-  const mockCallback = jest.fn((name, email) => { })
+  const mockCallback = jest.fn()
   const { getByPlaceholderText, getByRole } = render(<UserForm user={user} handleSubmit={mockCallback} />)
 
   fireEvent.change(getByPlaceholderText('Name'), { target: { value: 'Jane Doe' } })
