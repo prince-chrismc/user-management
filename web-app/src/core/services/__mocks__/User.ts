30cb8a1b6740016aed22f63155f40e8c260861de
chris <prince.chrismc@gmail.com>
2021-02-19 23:08:12 -0500

    remove jsx attribution from core functions

--- web-app/src/core/services/__mocks__/User.ts
+++ web-app/src/core/services/__mocks__/User.ts
@@ -3,5 +3,5 @@ export const EditUser = async ([id, name, email, etag], props, { signal }) => {
 }
 
 export const DeleteUser = async ([id, etag], props, { signal }) => {
-  return new Promise(resolve => resolve())
+  return new Promise(resolve => resolve(null))
 }
