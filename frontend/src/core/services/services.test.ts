f6e6f169d38683ce79a2a6c50dd5034020c529b6
chris <prince.chrismc@gmail.com>
2023-02-10 21:25:10 -0800

    first POC using nock instead

--- frontend/src/core/services/services.test.ts
+++ frontend/src/core/services/services.test.ts
@@ -4,10 +4,7 @@ import { LoadUsers, AddUser } from './List'
 import { EditUser, DeleteUser } from './User'
 import { Etag } from '../tools/Etag'
 
-import e, { json } from 'express'
-import { createHttpTerminator } from 'http-terminator'
-const app = e()
-const port = 3001
+import nock from 'nock'
 
 let __error = false
 
@@ -26,127 +23,115 @@ const JSON_USER_LIST = [
   JSON_USER_256
 ]
 
-app.use(json())
+// app.use(json())
 
-app.get('/um/v1/users', (req, res) => {
-  console.log('GET /um/v1/users')
+// app.get('/um/v1/users', (req, res) => {
+//   console.log('GET /um/v1/users')
 
-  if (__error) {
-    res.sendStatus(500)
-  } else {
-    res.send(JSON.stringify(JSON_USER_LIST))
-  }
-})
+//   if (__error) {
+//     res.sendStatus(500)
+//   } else {
+//     res.send(JSON.stringify(JSON_USER_LIST))
+//   }
+// })
 
-app.put('/um/v1/users', (req, res) => {
-  console.log('POST /um/v1/users', req.body)
+// app.put('/um/v1/users', (req, res) => {
+//   console.log('POST /um/v1/users', req.body)
 
-  if (__error) {
-    res.sendStatus(500)
-  } else {
-    const json = req.body
-    json.id = 543
-    res.send(JSON.stringify(json))
-  }
-})
+//   if (__error) {
+//     res.sendStatus(500)
+//   } else {
+//     const json = req.body
+//     json.id = 543
+//     res.send(JSON.stringify(json))
+//   }
+// })
 
-app.delete('/um/v1/users/:id(\\d+)', (req, res) => {
-  console.log('DELETE /um/v1/users/', req.params.id)
+// app.delete('/um/v1/users/:id(\\d+)', (req, res) => {
+//   console.log('DELETE /um/v1/users/', req.params.id)
 
-  if (__error) {
-    res.sendStatus(500)
-  } else if (typeof req.header('If-Match') === 'undefined') {
-    res.sendStatus(428)
-  } else {
-    res.sendStatus(204)
-  }
-})
+//   if (__error) {
+//     res.sendStatus(500)
+//   } else if (typeof req.header('If-Match') === 'undefined') {
+//     res.sendStatus(428)
+//   } else {
+//     res.sendStatus(204)
+//   }
+// })
 
-app.patch('/um/v1/users/:id(\\d+)', (req, res) => {
-  console.log('PATCH /um/v1/users/', req.params.id, req.body)
+// app.patch('/um/v1/users/:id(\\d+)', (req, res) => {
+//   console.log('PATCH /um/v1/users/', req.params.id, req.body)
 
-  if (__error) {
-    res.sendStatus(500)
-  } else if (typeof req.header('If-Match') === 'undefined') {
-    res.sendStatus(428)
-  } else {
-    const json = req.body
-    json.id = req.params.id
-    res.status(202).send(JSON.stringify(json))
-  }
-})
+//   if (__error) {
+//     res.sendStatus(500)
+//   } else if (typeof req.header('If-Match') === 'undefined') {
+//     res.sendStatus(428)
+//   } else {
+//     const json = req.body
+//     json.id = req.params.id
+//     res.status(202).send(JSON.stringify(json))
+//   }
+// })
 
-let httpTerminator = null
-
-beforeEach(async () => {
-  __error = false
-  const server = app.listen(port, () => {
-    console.log(`Mock listening at http://localhost:${port}`)
-  })
-  httpTerminator = createHttpTerminator({ server })
-
-  await new Promise(resolve => setTimeout(resolve, 1000))
-})
-
-afterEach(async () => {
-  httpTerminator.terminate()
-  await new Promise(resolve => setTimeout(resolve, 1000))
-})
 
 test('get list of users', async () => {
-  const json = await LoadUsers()
-  console.log(json)
+  const scope = nock('http://localhost:3001', {})
+    .get('/um/v1/users')
+    .reply(200, JSON_USER_LIST)
 
+  const json = await LoadUsers()
+
+  expect(scope.isDone())
   expect(json).toEqual(JSON_USER_LIST)
 })
 
-test('add new user to list', async () => {
-  const json = await AddUser(['James Does', 'james@example.com'], null, { signal: null })
-  console.log(json)
+// test('add new user to list', async () => {
+//   const json = await AddUser(['James Does', 'james@example.com'], null, { signal: null })
+//   console.log(json)
 
-  expect(json).toEqual({ id: 543, name: 'James Does', email: 'james@example.com' })
-})
+//   expect(json).toEqual({ id: 543, name: 'James Does', email: 'james@example.com' })
+// })
 
-test('faulty backend > get list', async () => {
-  __error = true
-  expect.assertions(1)
+// test('faulty backend > get list', async () => {
+//   __error = true
+//   expect.assertions(1)
 
-  await expect(LoadUsers()).rejects.toBeInstanceOf(Response)
-})
+//   await expect(LoadUsers()).rejects.toBeInstanceOf(Response)
+// })
 
-test('faulty backend > add user', async () => {
-  __error = true
-  expect.assertions(1)
+// test('faulty backend > add user', async () => {
+//   __error = true
+//   expect.assertions(1)
 
-  await expect(AddUser(['James Does', 'james@example.com'], null, { signal: null })).rejects.toBeInstanceOf(Response)
-})
+//   await expect(AddUser(['James Does', 'james@example.com'], null, { signal: null })).rejects.toBeInstanceOf(Response)
+// })
 
-test('delete users', async () => {
-  const etag = Etag(JSON_USER_123.id, JSON_USER_123.name, JSON_USER_123.email)
-  const json = await DeleteUser([123, etag], null, { signal: null })
-  console.log(json)
+// test('delete users', async () => {
+//   const etag = Etag(JSON_USER_123.id, JSON_USER_123.name, JSON_USER_123.email)
+//   const json = await DeleteUser([123, etag], null, { signal: null })
+//   console.log(json)
 
-  expect(json).toBeNull()
-})
+//   expect(json).toBeNull()
+// })
 
-test('edit users', async () => {
-  const etag = Etag(JSON_USER_123.id, JSON_USER_123.name, JSON_USER_123.email)
-  const json = await EditUser([123, 'James Does', 'james@example.com', etag], null, { signal: null })
-  console.log(json)
+// test('edit users', async () => {
+//   const etag = Etag(JSON_USER_123.id, JSON_USER_123.name, JSON_USER_123.email)
+//   const json = await EditUser([123, 'James Does', 'james@example.com', etag], null, { signal: null })
+//   console.log(json)
 
-  expect(json).toEqual({ email: 'james@example.com', id: '123', name: 'James Does' })
-})
+//   expect(json).toEqual({ email: 'james@example.com', id: '123', name: 'James Does' })
+// })
 
-test('faulty backend > delete user', async () => {
-  __error = true
-  expect.assertions(1)
+// test('faulty backend > delete user', async () => {
+//   __error = true
+//   expect.assertions(1)
 
-  await expect(DeleteUser([123, 'etag'], null, { signal: null })).rejects.toBeInstanceOf(Response)
-})
+//   await expect(DeleteUser([123, 'etag'], null, { signal: null })).rejects.toBeInstanceOf(Response)
+// })
 
-test('faulty backend > edit user', async () => {
-  __error = true
-  expect.assertions(1)
+// test('faulty backend > edit user', async () => {
+//   __error = true
+//   expect.assertions(1)
 
-  await expect(EditUser([123, 'James Does', 'james@example.com', 'etag'], null, { signal: null })).rejects.toBeInstanceOf(Response)
-})
+//   await expect(EditUser([123, 'James Does', 'james@example.com', 'etag'], null, { signal: null })).rejects.toBeInstanceOf(Response)
+// })
