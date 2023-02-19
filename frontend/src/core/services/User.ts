e17cd377000688836fa289b6e2645bb142eda0c9
chris <prince.chrismc@gmail.com>
2023-02-18 21:31:50 -0800

    finish mock services tests

--- frontend/src/core/services/User.ts
+++ frontend/src/core/services/User.ts
@@ -9,10 +9,12 @@ export const EditUser = async ([id, name, email, etag], props, { signal }) => {
     .then(res => res.json())
 }
 
+export const FormatEtag = (etag) => '"' + etag + '"'
+
 export const DeleteUser = async ([id, etag], props, { signal }) => {
   const requestOptions = {
     method: 'DELETE',
-    headers: { 'If-Match': '"' + etag + '"' }
+    headers: { 'If-Match': FormatEtag(etag) }
   }
   return await fetch(process.env.API_URL + '/um/v1/users/' + id, requestOptions)
     .then(res => (res.status === 204 ? null : Promise.reject(res)))
--- frontend/src/core/services/services.test.ts
+++ frontend/src/core/services/services.test.ts
@@ -1,7 +1,7 @@
 import 'cross-fetch/polyfill'
 
 import { LoadUsers, AddUser } from './List'
-import { EditUser, DeleteUser } from './User'
+import { EditUser, DeleteUser, FormatEtag } from './User'
 import { Etag } from '../tools/Etag'
 
 import nock from 'nock'
@@ -11,69 +11,14 @@ const JSON_USER_123 = {
   name: 'John Doe',
   email: 'john@example.com'
 }
-const JSON_USER_256 = {
-  id: 256,
-  name: 'Jane Doe',
-  email: 'jane@example.com'
-}
-const JSON_USER_LIST = [
-  JSON_USER_123,
-  JSON_USER_256
-]
-
-// app.use(json())
-
-// app.get('/um/v1/users', (req, res) => {
-//   console.log('GET /um/v1/users')
-
-//   if (__error) {
-//     res.sendStatus(500)
-//   } else {
-//     res.send(JSON.stringify(JSON_USER_LIST))
-//   }
-// })
-
-// app.put('/um/v1/users', (req, res) => {
-//   console.log('POST /um/v1/users', req.body)
-
-//   if (__error) {
-//     res.sendStatus(500)
-//   } else {
-//     const json = req.body
-//     json.id = 543
-//     res.send(JSON.stringify(json))
-//   }
-// })
-
-// app.delete('/um/v1/users/:id(\\d+)', (req, res) => {
-//   console.log('DELETE /um/v1/users/', req.params.id)
-
-//   if (__error) {
-//     res.sendStatus(500)
-//   } else if (typeof req.header('If-Match') === 'undefined') {
-//     res.sendStatus(428)
-//   } else {
-//     res.sendStatus(204)
-//   }
-// })
-
-// app.patch('/um/v1/users/:id(\\d+)', (req, res) => {
-//   console.log('PATCH /um/v1/users/', req.params.id, req.body)
-
-//   if (__error) {
-//     res.sendStatus(500)
-//   } else if (typeof req.header('If-Match') === 'undefined') {
-//     res.sendStatus(428)
-//   } else {
-//     const json = req.body
-//     json.id = req.params.id
-//     res.status(202).send(JSON.stringify(json))
-//   }
-// })
-
 
 test('get list of users', async () => {
-  const scope = nock('http://localhost:3001', {})
+  const JSON_USER_LIST = [JSON_USER_123, { id: 256, name: 'Jane Doe', email: 'jane@example.com' }]
+  const scope = nock('http://localhost:3001', {
+    reqheaders: {
+      'accept': 'application/json'
+    }
+  })
     .get('/um/v1/users')
     .reply(200, JSON_USER_LIST)
 
@@ -84,16 +29,17 @@ test('get list of users', async () => {
 })
 
 test('add new user to list', async () => {
-  const newUser = { id: 543, name: 'James Does', email: 'james@example.com' }
+  const newPerson = { name: 'James Does', email: 'james@example.com' }
+  const newUser = { id: 543, ...newPerson }
   const scope = nock('http://localhost:3001', {
     reqheaders: {
       'content-type': 'application/json'
     }
   })
-    .put('/um/v1/users', JSON.stringify({ name: 'James Does', email: 'james@example.com' }))
+    .put('/um/v1/users', JSON.stringify(newPerson))
     .reply(200, newUser)
 
-  const json = await AddUser(['James Does', 'james@example.com'], null, { signal: null })
+  const json = await AddUser([newPerson.name, newPerson.email], null, { signal: null })
 
   expect(scope.isDone())
   expect(json).toEqual(newUser)
@@ -130,9 +76,9 @@ test('faulty backend > add user', async () => {
 test('delete users', async () => {
   const etag = Etag(JSON_USER_123.id, JSON_USER_123.name, JSON_USER_123.email)
   const scope = nock('http://localhost:3001', {
-    // reqheaders: {
-    //   'if-match': etag
-    // }
+    reqheaders: {
+      'if-match': FormatEtag(etag)
+    }
   })
     .delete('/um/v1/users/123')
     .reply(204)
@@ -143,24 +89,46 @@ test('delete users', async () => {
   expect(json).toBeNull()
 })
 
-// test('edit users', async () => {
-//   const etag = Etag(JSON_USER_123.id, JSON_USER_123.name, JSON_USER_123.email)
-//   const json = await EditUser([123, 'James Does', 'james@example.com', etag], null, { signal: null })
-//   console.log(json)
+test('edit users', async () => {
+  const etag = Etag(JSON_USER_123.id, JSON_USER_123.name, JSON_USER_123.email)
+  const scope = nock('http://localhost:3001', {
+    reqheaders: {
+      'if-match': FormatEtag(etag)
+    }
+  })
+    .patch('/um/v1/users/123', JSON.stringify({ name: 'James Does', email: 'james@example.com' }))
+    .reply(202, { email: 'james@example.com', id: '123', name: 'James Does' })
 
-//   expect(json).toEqual({ email: 'james@example.com', id: '123', name: 'James Does' })
-// })
+  const json = await EditUser([123, 'James Does', 'james@example.com', etag], null, { signal: null })
 
-// test('faulty backend > delete user', async () => {
-//   __error = true
-//   expect.assertions(1)
+  expect(scope.isDone())
+  expect(json).toEqual({ email: 'james@example.com', id: '123', name: 'James Does' })
+})
 
-//   await expect(DeleteUser([123, 'etag'], null, { signal: null })).rejects.toBeInstanceOf(Response)
-// })
+test('faulty backend > delete user', async () => {
+  const scope = nock('http://localhost:3001', {
+    reqheaders: {
+      'if-match': 'etag'
+    }
+  })
+    .delete('/um/v1/users/123')
+    .reply(500)
 
-// test('faulty backend > edit user', async () => {
-//   __error = true
-//   expect.assertions(1)
+  await expect(DeleteUser([123, 'etag'], null, { signal: null })).rejects.toBeDefined()
 
-//   await expect(EditUser([123, 'James Does', 'james@example.com', 'etag'], null, { signal: null })).rejects.toBeInstanceOf(Response)
-// })
+  expect(scope.isDone())
+})
+
+test('faulty backend > edit user', async () => {
+  const scope = nock('http://localhost:3001', {
+    reqheaders: {
+      'if-match': 'etag'
+    }
+  })
+    .patch('/um/v1/users/123', JSON.stringify({ name: 'James Does', email: 'james@example.com' }))
+    .reply(500)
+
+  await expect(EditUser([123, 'James Does', 'james@example.com', 'etag'], null, { signal: null })).rejects.toBeDefined()
+
+  expect(scope.isDone())
+})
